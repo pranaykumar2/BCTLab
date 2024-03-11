864ab7ea34aad221d243ca6ec14319279980d966
Pranay Kumar <151812978+pranaykumar2@users.noreply.github.com>
2024-03-11 08:41:18 +0530

    Update RetrievePhno.sol

--- RetrievePhno.sol
+++ RetrievePhno.sol
@@ -4,13 +4,18 @@ pragma solidity >0.8.0;
 
 contract RetrievePhno {
 
-    address public owner;
+    address public contract_owner;
+    address public ph_owner;
+
+    constructor(){
+        contract_owner = msg.sender;
+    }
 
     mapping(address => string) public phoneNumbers;
 
     function setPhno(string memory _number, address _owner) public {
-        owner = _owner;
-        phoneNumbers[owner] =  _number;
+        ph_owner = _owner;
+        phoneNumbers[ph_owner] =  _number;
     }
 
     function getPhno(address _user) public view returns(string memory) {

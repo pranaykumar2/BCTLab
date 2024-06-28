142b29c5685642f6952185584b6d3934b2f81896
Pranay Kumar <151812978+pranaykumar2@users.noreply.github.com>
2024-06-28 07:52:59 +0530

    Update MerkleTress.sol

--- MerkleTress.sol
+++ MerkleTress.sol
@@ -1,31 +1,25 @@
-// SPDX-LICENSCE IDENTIFIER: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
 contract MerkleTree {
-    bytes32 public stateRoot;
-    bytes32 public transactionRoot;
-    bytes32 public receiptRoot;
+    uint256 public stateRoot;
+    uint256 public transactionRoot;
+    uint256 public receiptRoot;
 
-    constructor(bytes32[] memory stateLeaves, bytes32[] memory transactionLeaves, bytes32[] memory receiptLeaves) {
+    constructor(uint256[] memory stateLeaves, uint256[] memory transactionLeaves, uint256[] memory receiptLeaves) {
         stateRoot = computeMerkleRoot(stateLeaves);
         transactionRoot = computeMerkleRoot(transactionLeaves);
         receiptRoot = computeMerkleRoot(receiptLeaves);
     }
 
-    function computeMerkleRoot(bytes32[] memory leaves) public pure returns (bytes32) {
-        bytes32 root = leaves[0];
+    function computeMerkleRoot(uint256[] memory leaves) public pure returns (uint256) {
+        uint256 root = uint256(leaves[0]);
         for (uint i = 1; i < leaves.length; i++) {
-            root = keccak256(abi.encodePacked(root, leaves[i]));
+            root = uint256(keccak256(abi.encodePacked(root, leaves[i])));
         }
         return root;
     }
 
-    function verifyProof(bytes32[] memory proof, bytes32 leaf, bytes32 root) public pure returns (bool) {
-        bytes32 computedHash = leaf;
-        for (uint i = 0; i < proof.length; i++) {
-            computedHash = keccak256(abi.encodePacked(computedHash, proof[i]));
-        }
-        return computedHash == root;
-    }
+    
 }
